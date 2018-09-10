5183c093c27afd9d4f08854dc1e18d09e0fdc889
Diego Biazin <diego_biazin@hotmail.com>
2018-09-10 20:39:06 -0300

    Criação layout Register

---
+++ app/src/main/java/com/diegobiazin/task/RegisterActivity.kt
@@ -0,0 +1,32 @@
+package com.diegobiazin.task
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        setListeners()
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.buttonSave -> {
+                handleSave()
+            }
+        }
+    }
+
+    private fun handleSave() {
+
+    }
+
+    private fun setListeners(){
+        buttonSave.setOnClickListener(this)
+    }
+}
