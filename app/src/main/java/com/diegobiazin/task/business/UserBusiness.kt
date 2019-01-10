21a313989638a5aa7c1053cd6775fb3db5e0d49a
Diego Biazin <diego_biazin@hotmail.com>
2019-01-10 17:30:58 -0200

    Salvando dados de autenticação

--- app/src/main/java/com/diegobiazin/task/business/UserBusiness.kt
+++ app/src/main/java/com/diegobiazin/task/business/UserBusiness.kt
@@ -2,13 +2,16 @@ package com.diegobiazin.task.business
 
 import android.content.Context
 import com.diegobiazin.task.R
+import com.diegobiazin.task.constants.TaskConstants
 import com.diegobiazin.task.repository.UserRepository
+import com.diegobiazin.task.util.SecurityPreferences
 import com.diegobiazin.task.util.ValidationException
 import java.lang.Exception
 
 class UserBusiness(var context: Context) {
 
     private val mUserRepository: UserRepository = UserRepository.getInstance(context)
+    private val mSecurityPreferences: SecurityPreferences = SecurityPreferences(context)
 
     fun insert(name: String, email: String, password: String) {
 
@@ -16,10 +19,15 @@ class UserBusiness(var context: Context) {
             if (name == "" || email == "" || password == "")
                 throw ValidationException(context.getString(R.string.informe_todos_campos))
 
-            if(mUserRepository.isEmailExistent(email))
+            if (mUserRepository.isEmailExistent(email))
                 throw ValidationException(context.getString(R.string.email_em_uso))
 
             val userId = mUserRepository.insert(name, email, password)
+
+            // Salvar dados do usuário na shared
+            mSecurityPreferences.storeString(TaskConstants.KEY.USER_ID, userId.toString())
+            mSecurityPreferences.storeString(TaskConstants.KEY.USER_NAME, name)
+            mSecurityPreferences.storeString(TaskConstants.KEY.USER_EMAIL, email)
         } catch (e: Exception) {
             throw e
         }
---
+++ app/src/main/java/com/diegobiazin/task/constants/TaskConstants.kt
@@ -0,0 +1,10 @@
+package com.diegobiazin.task.constants
+
+class TaskConstants {
+
+    object KEY {
+        val USER_ID = "userId"
+        val USER_NAME = "userName"
+        val USER_EMAIL = "userEmail"
+    }
+}
