80181bf56d71557235990337468da989e0fdf195
Diego Biazin <diego_biazin@hotmail.com>
2019-01-11 16:09:15 -0200

    Criando Activity de cadastro de tarefas

---
+++ app/src/main/java/com/diegobiazin/task/views/TaskFormActivity.kt
@@ -0,0 +1,13 @@
+package com.diegobiazin.task.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.diegobiazin.task.R
+
+class TaskFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_form)
+    }
+}
